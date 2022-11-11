452ac39b3e3e33a5cbe83be832203f2fc6064093
iimad <alisawiali70@gmail.com>
2022-11-11 11:02:51 +0100

    function

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+function pro(price,taxes,ads){
+    let main = price+ taxes +ads;
+    let result=main;
+   
+   
+ 
+    return result;
+}
+
+function calcAge (age){
+    let  calc = age *365;
+    let result1 = calc;
+    return result1;
+
+}
+ let e =pro(100, 2.2,10)
+ let u =calcAge(30)
+
+ console.log(e)
+ console.log(u)
