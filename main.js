c72a442bc8e8f805ad35f8c285895ce26863a7a3
iimad <alisawiali70@gmail.com>
2022-11-11 14:28:21 +0100

    argument push

--- main.js
+++ main.js
@@ -20,3 +20,36 @@ function calcAge (age){
 
  console.log(e)
  console.log(u)
+
+
+ console.log('-------------')
+
+
+ let m =['hallo', 'Omer']
+
+ function hallo( ...m ){
+    let result = 0;
+
+    for (let x =0; x < m.length; x++)
+    {
+  
+       result += m[x]
+    }
+    
+   console.log(result)
+    
+ }
+ hallo ( 1,2,3,4,5);
+ console.log('------------zweite beispiel----------------')
+
+ let mal =[' Omer' , 'Amer', ' mustfa']
+
+function Mari( ...mal){
+    let result =0;
+    for(let d =0; d < mal.length; d++){
+        result += mal[d];
+    }
+    console.log(result)
+}
+
+Mari( 1,3,5,1)
